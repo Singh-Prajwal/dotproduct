91a3f81f235ca6c747052ea809f00db16bb236b0
Prajwal Singh <prajwal.singh.226@gmail.com>
2025-05-23 22:49:07 +0530

    Fix: Render Deploy

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
-set -o errexit  # Exit immediately if a command exits with a non-zero status.
+set -o errexit
 
+# Change to the directory containing manage.py
+cd /opt/render/project/src
+
+# Upgrade pip and install requirements
+python -m pip install --upgrade pip
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Run migrations
 python manage.py migrate
